19df4269c654e5a281654dfdf995ec8836161759
Boris-Bodin <boris.bodin@aegis-techno.fr>
2023-10-07 18:09:08 +0200

    Step 1

--- src/test/java/org/example/MultiCurrencyAppTest.java
+++ src/test/java/org/example/MultiCurrencyAppTest.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.example.money.Dollar;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MultiCurrencyAppTest {
+
+    @Test
+    public void testMultipication() {
+        Dollar dollar = new Dollar(5);
+        dollar.times(2);
+        assertEquals(10, dollar.amount);
+    }
 }
